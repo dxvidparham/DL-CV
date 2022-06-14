5e1d1e7eb6984c941bed21bf78276f4b03a1a943
Alex <alex.martinez01250@gmail.com>
2022-06-14 17:48:55 +0200

    interpolation out

--- interpolation.py
+++ interpolation.py
@@ -3,6 +3,7 @@ import torch
 import pickle
 import PIL.Image
 import numpy as np
+import matplotlib.pyplot as plt 
 
 
 outdir = "out"
@@ -31,7 +32,25 @@ for idx, w in enumerate(v1):
     img = G.synthesis(w.unsqueeze(0), noise_mode="const")
     img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
     img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/Alex_projection{idx:02d}.png')
+    x_alex = img[0].cpu().numpy()
 for idx, w in enumerate(v2):
     img = G.synthesis(w.unsqueeze(0), noise_mode="const")
     img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
     img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/Chris_projection{idx:02d}.png')
+    x_chris = img[0].cpu().numpy()
+
+plt.figure(figsize=(20,20))
+subplots = [plt.subplot(2, 5, k+1) for k in range(10)]
+subplots[0].imshow(x_alex)
+subplots[0].set_title('Alex')
+subplots[0].axis('off')
+
+subplots[9].imshow(x_chris)
+subplots[9].set_title('Chris')
+subplots[9].axis('off')
+for k in range(1,10):
+    x_k = G.synthesis(torch.lerp(v1[0].unsqueeze(0),v2[0].unsqueeze(0),k/10), noise_mode="const")
+    subplots[k].imshow(x_k[0])
+    subplots[k].set_title('middleground' % k)
+    subplots[k].axis('off')
+plt.savefig("out/interpolation.png")
