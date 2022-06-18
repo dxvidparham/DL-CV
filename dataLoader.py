dea8504be1a6b1b667992fa0a246cc58586b98a7
david <s202385@student.dtu.dk>
2022-06-18 16:44:32 +0200

    Fixed dataloader index out of bounce error, due to multiple segmentations per image

--- dataLoader.py
+++ dataLoader.py
@@ -47,8 +47,18 @@ class ISICDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         "Generates one sample of data"
-        image_path = self.image_paths[idx]
         mask_path = self.mask_paths[idx]
+
+        try:
+            image_path = self.image_paths[idx]
+        except IndexError:
+            search_string = mask_path.split("_seg")[0]
+            for i, path in enumerate(self.image_paths):
+                if search_string in path:
+                    idx = i
+                    break
+
+            image_path = self.image_paths[idx]
 
         image = np.array(Image.open(image_path))
         mask = np.array(Image.open(mask_path))
